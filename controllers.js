a896a6b4c6e4d8a5293891c97ac55b7fb15b4fda
dipadana <putudipadana@gmail.com>
2019-09-12 15:58:09 +0700

    Kumpul Countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,19 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+
+  // let detik = 20
+
+    const timer = setInterval(function(){
+      seconds = seconds-1
+      displayCountdown(seconds)
+      if(seconds == 0){
+        clearInterval(timer)
+      }
+    },1000)
+
+
+
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,9 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  
+  let detikSaatIni = (new Date(seconds * 1000).toISOString().substr(11, 8))
+  console.log(chalk.cyan(figlet.textSync(`${detikSaatIni}`)));
 };
 
 module.exports = { displayCountdown };
