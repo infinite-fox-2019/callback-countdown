3fca1519db3e57bdfb8b319f6a71b50345d94d79
aldinofrizal <aldinofrizal@gmail.com>
2019-09-12 18:17:44 +0700

    countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,16 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let waktuDalamDetik = seconds
+  let interval = setInterval(() => {
+    waktuDalamDetik--
+    let menit = Math.floor(waktuDalamDetik / 60)
+    let detik = waktuDalamDetik % 60
+    displayCountdown(menit , detik)
+    if(waktuDalamDetik === 0)clearInterval(interval)
+  }, 1000)
+  
+  
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,9 +2,11 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (menit,detik) => {
   clear();
+
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync(`${menit}: ${detik}`)));
   // Your code here...
 };
 
