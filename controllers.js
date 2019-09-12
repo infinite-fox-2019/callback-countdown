6fcfefbc22392436232f3b41eacdd49c4f74f011
arnold_therigan <arnoldtherigan15@gmail.com>
2019-09-12 16:09:25 +0700

    finished

--- controllers.js
+++ controllers.js
@@ -1,9 +1,20 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds => {
-  // Your code here...
-};
 
+const timer = seconds => {
+  let play = setInterval(function(){
+    if (seconds === 0) {
+      displayCountdown(seconds);
+      clearInterval(play);
+    } else {
+      displayCountdown(seconds);
+      seconds--;
+    }
+  },500)
+};
+// const timer = function(seconds) {
+
+// }
 module.exports = {
   timer
 };
--- views.js
+++ views.js
@@ -3,8 +3,17 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 
 const displayCountdown = seconds => {
-  clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  // clear();
+  // console.log(chalk.yellow.bold(figlet.textSync("Happy New Year")));
+  // console.log(chalk.blue.bold(figlet.textSync(seconds)));
+  if (seconds === 0) {
+    clear();
+    console.log(chalk.red.bgYellow.bold(figlet.textSync("Happy New Year")));
+  } else {
+    clear();
+    console.log(chalk.yellow.bold(figlet.textSync("Ayo Menghitung")));
+    console.log(chalk.blue.bold(figlet.textSync(seconds)));
+  }
   // Your code here...
 };
 
