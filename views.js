324f267d0f89fe61a410b5aa4c6ef869bdee3b11
dadepe <rizky_dwifutra@yahoo.com>
2019-09-12 16:32:30 +0700

    adding countDown

--- views.js
+++ views.js
@@ -6,6 +6,9 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+
+  console.log(chalk.cyan(figlet.textSync(`${Math.floor((seconds%3600)/60)} : ${seconds%60}`)))
+  
 };
 
 module.exports = { displayCountdown };
