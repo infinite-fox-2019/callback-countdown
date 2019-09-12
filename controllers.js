0eb71c57362cd16aac8c10f09aa6283241089a75
evans <karlinevans333@gmail.com>
2019-09-12 16:09:24 +0700

    Callback Countdown Fisnish

--- controllers.js
+++ controllers.js
@@ -2,7 +2,18 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let myVar = setInterval(function (){
+    if (seconds == 0){
+      displayCountdown("AJA TEROSS!")
+      clearInterval(myVar)
+    }
+    else{
+      displayCountdown(seconds)
+      seconds--
+    }
+  } , 1000)
 };
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -4,7 +4,8 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.red.bold(figlet.textSync("Loading")));
+  console.log(chalk.yellow.bold(figlet.textSync(seconds)));
   // Your code here...
 };
 
