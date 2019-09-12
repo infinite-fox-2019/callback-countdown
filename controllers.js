2d5a0cea3cdc500a718ac026bfbfe06d6bebedfd
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-12 18:11:14 +0700

    mengumpulkan tugas countdown

--- controllers.js
+++ controllers.js
@@ -1,9 +1,24 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let interval = setInterval(()=>{
+    let menit = Math.floor(seconds/60)
+    if(menit<10){
+      menit = '0'+menit
+    }
+    let second = seconds%60
+    if(second<10){
+      second = '0'+second
+    }
+    let test = ''
+    test+= menit +':'+second
+    displayCountdown(test)
+    seconds-=1
+    if(seconds < 0){
+      clearInterval(interval)
+    }
+  },1000)
 };
-
 module.exports = {
   timer
 };
