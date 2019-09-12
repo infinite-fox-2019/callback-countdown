946d5b261cb570fa829bccadebeba7731edb066e
Peter Lee <peter.lee230597@gmail.com>
2019-09-12 23:46:09 +0700

    Countdown

--- controllers.js
+++ controllers.js
@@ -2,8 +2,21 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let countdown = setInterval(function(){
+    // let minutes = Math.floor(seconds/ 60)
+    // let detik = seconds % 60
+    seconds--
+    displayCountdown(seconds)
+    if(seconds === 0){
+      clearInterval(countdown)
+    }
+  }, 1000)
 };
 
 module.exports = {
   timer
 };
+
+
+//340
+// minutes: 5
