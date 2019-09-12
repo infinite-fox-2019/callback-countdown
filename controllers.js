53bcb243f892e3132d24d7b9ee3e5e16b91756bb
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-12 16:49:14 +0700

    MM:DD format

--- controllers.js
+++ controllers.js
@@ -2,6 +2,12 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let countdownInterval = setInterval(function(){
+    if(seconds === 0) {
+      clearInterval(countdownInterval);
+    }
+    displayCountdown(seconds--);
+  }, 1000);
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,18 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (seconds) => {
   clear();
+  let minutes = Math.floor(seconds / 60);
+  if(minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  seconds = seconds % 60;
+  if(seconds < 10) {
+    seconds = '0' + seconds;
+  }
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync(minutes + ':' + seconds)));
 };
 
 module.exports = { displayCountdown };
