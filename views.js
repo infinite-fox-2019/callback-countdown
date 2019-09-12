e19802b864f28047441ba14a7ab73ac03c582a49
dipadana <putudipadana@gmail.com>
2019-09-12 17:01:28 +0700

    Update view format

--- views.js
+++ views.js
@@ -6,8 +6,8 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
-  
-  let detikSaatIni = (new Date(seconds * 1000).toISOString().substr(11, 8))
+
+  let detikSaatIni = (new Date(seconds * 1000).toISOString().substr(14, 5))
   console.log(chalk.cyan(figlet.textSync(`${detikSaatIni}`)));
 };
 
