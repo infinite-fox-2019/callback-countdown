0ae679b31794ee2a832cef605e809e3e62757a48
anggabanny <anggabanny@gmail.com>
2019-09-12 16:01:55 +0700

    Up

--- controllers.js
+++ controllers.js
@@ -2,8 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let timing = setInterval(function(){
+    if(seconds === 0) {
+      clearInterval(timing)
+    }
+    displayCountdown(seconds)
+    seconds--
+  },1000)
 };
 
-module.exports = {
-  timer
-};
+module.exports = {timer};
--- views.js
+++ views.js
@@ -2,9 +2,14 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
+
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  var date = new Date(null);
+  date.setSeconds(seconds); // specify value for SECONDS here
+  var timeString = date.toISOString().substr(11, 8);
+  console.log(chalk.cyan(figlet.textSync(timeString)));
   // Your code here...
 };
 
