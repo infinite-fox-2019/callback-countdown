5976c4a4cf0e14b1c8796372304b10ab9aa13532
Afifah Rahma Kustanto <52028283+afifahrahmak@users.noreply.github.com>
2019-09-12 18:12:16 +0700

    Update views.js

--- views.js
+++ views.js
@@ -4,12 +4,16 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.blue(figlet.textSync("Countdown App")));
-  if (seconds%60 < 10){
-    console.log(chalk.yellow(figlet.textSync(`${Math.floor(seconds/60)} : 0${Math.round(seconds%60)}`)))
-  }else{
-    console.log(chalk.yellow(figlet.textSync(`${Math.floor(seconds/60)} : ${Math.round(seconds%60)}`)))
+  let menit = Math.floor(seconds/60)
+  if (menit < 10){
+    menit = `0${menit}`
   }
+  let detik = Math.round(seconds%60)
+  if (detik < 10){
+    detik = `0${detik}`
+  }
+  console.log(chalk.magenta(figlet.textSync("Countdown App")));
+  console.log(chalk.yellow(figlet.textSync(`${menit} : ${detik}`)))}
 };
 
 module.exports = { displayCountdown };
