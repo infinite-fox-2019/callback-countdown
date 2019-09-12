642bffd5cc32058b95c3697850e0014035330128
hoandreas <andreas_ho@hotmail.com>
2019-09-12 16:53:36 +0700

    submit callback countdown

--- controllers.js
+++ controllers.js
@@ -1,7 +1,22 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let start = setInterval(function(){
+    let displayMinutes = Math.floor(seconds / 60);
+    let displaySeconds = seconds % 60;
+    if(displayMinutes < 10) {
+      displayMinutes = "0" + displayMinutes;
+    }
+    if(displaySeconds < 10) {
+      displaySeconds = "0" + displaySeconds;
+    }
+    displayCountdown(`${displayMinutes} : ${displaySeconds}`)
+    seconds--;
+    if(seconds === 0) {
+      clearInterval(start);
+      displayCountdown('00:00');
+    }
+  },1000)
 };
 
 module.exports = {
